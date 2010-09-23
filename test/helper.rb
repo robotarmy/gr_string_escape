ffadde8662feda46709e0412e41293660eb2e823
Curtis Schofield <github.com@robotarmyma.de>
2010-09-22 17:13:41 -0700

    new assertion

--- test/helper.rb
+++ test/helper.rb
@@ -4,14 +4,9 @@ require 'test/unit'
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'gr_string_escape'
-
+require 'new_assertions'
 class Test::Unit::TestCase
-  
-  def assert_size(object, size)
-    assert object.size == size,
-      "#{object.inspect} was expected to be size #{size} " +
-      "but was size #{object.size}"
-  end
+  include NewAssertions
 end
 
 
